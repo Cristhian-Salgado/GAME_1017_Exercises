cf90bfb5389b786e396639bbb8dc6ee4653363aa
Cristhian-Salgado <92034504+Cristhian-Salgado@users.noreply.github.com>
2022-03-23 17:57:03 -0400

    Created PlatformPlayer.h

---
+++ LE4_SalgadoCristhian/GAME1017_EmptyEngine/PlatformPlayer.cpp
@@ -0,0 +1,60 @@
+#include "PlatformPlayer.h"
+
+PlatformPlayer::PlatformPlayer(SDL_Rect s, SDL_FRect d)
+{
+}
+
+void PlatformPlayer::Update()
+{
+}
+
+void PlatformPlayer::Render()
+{
+}
+
+void PlatformPlayer::Stop()
+{
+}
+
+void PlatformPlayer::StopX()
+{
+}
+
+void PlatformPlayer::StopY()
+{
+}
+
+void PlatformPlayer::SetAccelX(double a)
+{
+}
+
+void PlatformPlayer::SetAccely(double a)
+{
+}
+
+void PlatformPlayer::setX(float x)
+{
+}
+
+void PlatformPlayer::setY(float y)
+{
+}
+
+void PlatformPlayer::SetGrounded(bool g)
+{
+}
+
+bool PlatformPlayer::IsGrounded()
+{
+	return false;
+}
+
+double PlatformPlayer::GetVelX()
+{
+	return 0.0;
+}
+
+double PlatformPlayer::GetVelY()
+{
+	return 0.0;
+}
